461757039c24578a476c022ad0b8917535aaf5ef
mihansweatpants <mihansweatpants@gmail.com>
2020-07-09 21:01:17 +0300

    Add tests for traffic blocks task

--- csv-parsing/src/main/java/org/example/Main.java
+++ csv-parsing/src/main/java/org/example/Main.java
@@ -3,21 +3,36 @@ package org.example;
 import java.io.FileInputStream;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Scanner;
 
 public class Main {
+    public static String WRONG_NUM_OF_ARGUMENTS_ERR_MSG = "Need to specify path to csv file and date (dd.MM.yyyy)";
+    public static String WRONG_DATE_FORMAT_ERR_MSG = "Date should be in format: dd.MM.yyyy";
+
     public static void main(String[] args) {
         if (args.length < 2) {
-            throw new IllegalArgumentException("Need to specify path to csv file and date (dd.MM.yyyy)");
+            throw new IllegalArgumentException(WRONG_NUM_OF_ARGUMENTS_ERR_MSG);
         }
 
         String pathToCsvFile = args[0];
-        LocalDate inputDate = LocalDate.parse(args[1], DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        LocalDate inputDate = null;
 
+        try {
+            inputDate = LocalDate.parse(args[1], DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        }
+        catch (DateTimeParseException e) {
+            throw new IllegalArgumentException(WRONG_DATE_FORMAT_ERR_MSG);
+        }
+
+        System.out.printf("There were %d traffic blocks in %s", getNumberOfTrafficBlocks(pathToCsvFile, inputDate), args[1]);
+    }
+
+    public static int getNumberOfTrafficBlocks(String pathToCsvFile, LocalDate inputDate) {
         int numberOfTrafficBlocks = 0;
 
         try (FileInputStream inputStream = new FileInputStream(pathToCsvFile);
-            Scanner scanner = new Scanner(inputStream, "UTF-8")) {
+             Scanner scanner = new Scanner(inputStream, "UTF-8")) {
 
             DateTimeFormatter trafficBlockDateFormat = DateTimeFormatter.ofPattern("yyyyMMdd");
 
@@ -48,6 +63,6 @@ public class Main {
             System.out.println(e);
         }
 
-        System.out.printf("There were %d traffic blocks in %s", numberOfTrafficBlocks, args[1]);
+        return numberOfTrafficBlocks;
     }
 }
--- csv-parsing/src/test/java/org/example/MainTest.java
+++ csv-parsing/src/test/java/org/example/MainTest.java
@@ -1,4 +1,32 @@
 package org.example;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class MainTest {
+    @Test
+    @DisplayName("Main method throws when receiving incorrect arguments")
+    public void testArgs() {
+        var wrongNumberOfArgsException = assertThrows(IllegalArgumentException.class, () -> Main.main(new String[]{"some stuff"}));
+        assertEquals(Main.WRONG_NUM_OF_ARGUMENTS_ERR_MSG, wrongNumberOfArgsException.getMessage());
+
+        var wrongDateFormatException = assertThrows(IllegalArgumentException.class, () ->
+                Main.main(new String[] {"some_file.csv", "2020-01-01"}));
+        assertEquals(Main.WRONG_DATE_FORMAT_ERR_MSG, wrongDateFormatException.getMessage());
+    }
+
+    @Test
+    @DisplayName("Main.getNumberOfTrafficBlocks returns right amount")
+    public void testGetNumberOfTrafficBlocks() {
+        var dateFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+        assertEquals(Main.getNumberOfTrafficBlocks("data.csv", LocalDate.parse("01.01.2013", dateFormatter)), 4);
+        assertEquals(Main.getNumberOfTrafficBlocks("data.csv", LocalDate.parse("06.06.2017", dateFormatter)), 173);
+    }
 }
