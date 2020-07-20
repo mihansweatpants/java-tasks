2c474055e8598a6f74d6c5d5573b05401cfd981a
mihansweatpants <mihansweatpants@gmail.com>
2020-07-20 17:37:28 +0300

    Add emojis for weather conidtions in telegram-weather-bot task

--- telegram-weather-bot/src/main/java/org/example/WeatherBot.java
+++ telegram-weather-bot/src/main/java/org/example/WeatherBot.java
@@ -23,7 +23,11 @@ public class WeatherBot extends TelegramLongPollingBot {
                     var weather = weatherApi.getWeatherByGeoLocation(location.getLatitude(), location.getLongitude());
 
                     SendMessage response = new SendMessage()
-                            .setText(String.format("Current weather for %s:\n%s, %s°C", weather.locationName, weather.weatherConditions.get(0).description, weather.weatherAttributes.temperature))
+                            .setText(String.format("Current weather for %s:\n%s %s, %s°C",
+                                    weather.locationName,
+                                    weatherDescriptionToEmoji(weather.weatherConditions.get(0).description),
+                                    weather.weatherConditions.get(0).description,
+                                    weather.weatherAttributes.temperature))
                             .setChatId(update.getMessage().getChatId());
 
                     execute(response);
@@ -43,5 +47,42 @@ public class WeatherBot extends TelegramLongPollingBot {
     @Override
     public String getBotToken() {
         return System.getenv("BOT_TOKEN");
+    }
+
+    private String weatherDescriptionToEmoji(String description) {
+        String emoji = "";
+
+        switch (description) {
+            case "clear sky":
+                emoji = "\u2600";
+                break;
+
+            case "scattered clouds":
+                emoji = "\u26C5";
+                break;
+
+            case "broken clouds":
+                emoji = "\u2601";
+                break;
+
+            case "rain":
+            case "shower rain":
+                emoji = "\uD83C\uDF27";
+                break;
+
+            case "thunderstorm":
+                emoji = "\u26C8";
+                break;
+
+            case "snow":
+                emoji = "\uD83C\uDF28";
+                break;
+
+            case "mist":
+                emoji = "\uD83C\uDF2B";
+                break;
+        }
+
+        return emoji;
     }
 }
