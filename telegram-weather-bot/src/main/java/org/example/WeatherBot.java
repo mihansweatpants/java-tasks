eb5d05f435e815db989f0eff395f3a814600ca33
mihansweatpants <mihansweatpants@gmail.com>
2020-07-30 16:04:50 +0300

    Complete telegram-weather-bot-task

--- telegram-weather-bot/src/main/java/org/example/WeatherBot.java
+++ telegram-weather-bot/src/main/java/org/example/WeatherBot.java
@@ -10,13 +10,25 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.ZoneId;
+import java.time.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 public class WeatherBot extends TelegramLongPollingBot {
+    private final String SUBSCRIBE_COMMAND = "/subscribe";
+    private final String UNSUBSCRIBE_COMMAND = "/unsubscribe";
+    private final String ASK_FOR_LOCATION_TEXT = "Please reply to this message with your location";
+
     private final WeatherApi weatherApi;
+
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+    private final List<ForecastSubscription> subscriptions = new ArrayList<>();
 
     public WeatherBot(WeatherApi weatherApi) {
         this.weatherApi = weatherApi;
@@ -25,14 +37,29 @@ public class WeatherBot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
         if (update.hasMessage()) {
-            if (update.getMessage().hasLocation()) {
-                Location location = update.getMessage().getLocation();
+            try {
+                if (update.getMessage().isCommand()) {
+                    if (update.getMessage().getText().equals(SUBSCRIBE_COMMAND)) {
+                        subscribeToForecasts(update.getMessage().getChatId());
+                        return;
+                    }
 
-                try {
-                    sendCurrentForecast(location, update.getMessage().getChatId());
-                } catch (Exception e) {
-                    e.printStackTrace();
+                    if (update.getMessage().getText().equals(UNSUBSCRIBE_COMMAND)) {
+                        unsubscribeFromForecasts(update.getMessage().getChatId());
+                        return;
+                    }
                 }
+
+                if (update.getMessage().hasLocation()) {
+                    if (update.getMessage().isReply() && update.getMessage().getReplyToMessage().getText().equals(ASK_FOR_LOCATION_TEXT)) {
+                        subscribeToForecasts(update.getMessage().getChatId(), update.getMessage().getLocation());
+                        return;
+                    }
+
+                    sendCurrentForecast(update.getMessage().getChatId(), update.getMessage().getLocation());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
     }
@@ -47,7 +74,72 @@ public class WeatherBot extends TelegramLongPollingBot {
         return System.getenv("BOT_TOKEN");
     }
 
-    private void sendCurrentForecast(Location location, Long chatId) throws InterruptedException, IOException, URISyntaxException, TelegramApiException {
+    private void subscribeToForecasts(Long chatId) throws TelegramApiException {
+        if (isSubscriptionStillActive(chatId)) {
+            execute(new SendMessage(chatId, "You are already subscribed.\nUse /unsubscribe command first if you want to update your subscription."));
+            return;
+        }
+
+        execute(new SendMessage(chatId, ASK_FOR_LOCATION_TEXT));
+    }
+
+    private void subscribeToForecasts(Long chatId, Location location) throws InterruptedException, IOException, URISyntaxException, TelegramApiException {
+        var weatherApiResponse = weatherApi.getWeatherByGeoLocation(location.getLatitude(), location.getLongitude());
+
+        ForecastSubscription subscription = new ForecastSubscription(chatId, location, ZoneId.of(weatherApiResponse.timezone));
+        subscriptions.add(subscription);
+
+        scheduleNotifications(subscription);
+
+        execute(new SendMessage(chatId, "You will receive daily forecasts"));
+    }
+
+    private boolean isSubscriptionStillActive(Long chatId) {
+        return subscriptions.stream().anyMatch(subscription -> subscription.getChatId().equals(chatId));
+    }
+
+    private void unsubscribeFromForecasts(Long chatId) throws TelegramApiException {
+        removeSubscription(chatId);
+        execute(new SendMessage(chatId, "You will not receive daily forecasts anymore"));
+    }
+
+    private void scheduleNotifications(ForecastSubscription subscription) {
+        Runnable forecastNotificationTask = () -> {
+            try {
+                sendCurrentForecast(subscription.getChatId(), subscription.getLocation());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        };
+
+        ZonedDateTime now = ZonedDateTime.now(subscription.getTimezoneId());
+        ZonedDateTime nextRun = now.withHour(8).withMinute(0).withSecond(0);
+        if (now.compareTo(nextRun) > 0) nextRun = nextRun.plusDays(1);
+
+        long initialDelay = Duration.between(now, nextRun).getSeconds();
+
+        subscription.setScheduledNotificationTask(
+                scheduler.scheduleAtFixedRate(
+                        forecastNotificationTask,
+                        initialDelay,
+                        TimeUnit.DAYS.toSeconds(1),
+                        TimeUnit.SECONDS
+                )
+        );
+    }
+
+    private void removeSubscription(Long chatId) {
+        var subscriptionToRemove = subscriptions.stream()
+                .filter(subscription -> subscription.getChatId().equals(chatId))
+                .findAny()
+                .get();
+
+        subscriptionToRemove.getScheduledNotificationTask().cancel(true);
+
+        subscriptions.remove(subscriptionToRemove);
+    }
+
+    private void sendCurrentForecast(Long chatId, Location location) throws InterruptedException, IOException, URISyntaxException, TelegramApiException {
         var weatherApiResponse = weatherApi.getWeatherByGeoLocation(location.getLatitude(), location.getLongitude());
 
         StringBuilder sb = new StringBuilder();
@@ -107,3 +199,51 @@ public class WeatherBot extends TelegramLongPollingBot {
         return emoji;
     }
 }
+
+class ForecastSubscription {
+    private final Long chatId;
+    private final Location location;
+    private final ZoneId timezoneId;
+
+    private Future scheduledNotificationTask;
+
+    public ForecastSubscription(Long chatId, Location location, ZoneId timezoneId) {
+        this.chatId = chatId;
+        this.location = location;
+        this.timezoneId = timezoneId;
+    }
+
+    public void setScheduledNotificationTask(Future scheduledNotificationTask) {
+        this.scheduledNotificationTask = scheduledNotificationTask;
+    }
+
+    public Future getScheduledNotificationTask() {
+        return scheduledNotificationTask;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public Long getChatId() {
+        return chatId;
+    }
+
+    public ZoneId getTimezoneId() {
+        return timezoneId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ForecastSubscription that = (ForecastSubscription) o;
+        return Objects.equals(chatId, that.chatId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(chatId, location, timezoneId, scheduledNotificationTask);
+    }
+}
+
